8835465252fd89583061ede9ba82363d6399cc81
ilias.karditsis <elias.barbarosa@gmail.com>
2014-08-07 17:00:06 +0300

    Commit before leaving from work 07/08/2014

---
+++ app/src/main/java/MarketApp/barbarosa/marketshopapp/MarketApp/barbarosa/marketshopapp/NetworkUtilities/Operation.java
@@ -0,0 +1,7 @@
+package MarketApp.barbarosa.marketshopapp.MarketApp.barbarosa.marketshopapp.NetworkUtilities;
+
+/**
+ * Created by ilias.karditsis on 7/8/2014.
+ */
+public class Operation {
+}
---
+++ app/src/main/java/MarketApp/barbarosa/marketshopapp/MarketApp/barbarosa/marketshopapp/NetworkUtilities/Tables.java
@@ -0,0 +1,10 @@
+package MarketApp.barbarosa.marketshopapp.MarketApp.barbarosa.marketshopapp.NetworkUtilities;
+
+/**
+ * Created by ilias.karditsis on 7/8/2014.
+ */
+public enum Tables {
+
+    EMPLOYEES, MARKETS, MARKETS_LOCATIONS, MARKETS_MAPS, ORDER_DETAILS, ORDERS, PRODUCT_CATEGORIES, PRODUCT_CATEGORIES_DETAILS, PRODUCTS, USERS, USERS_CREDENTIALS, USERS_LOCATIONS
+
+}
