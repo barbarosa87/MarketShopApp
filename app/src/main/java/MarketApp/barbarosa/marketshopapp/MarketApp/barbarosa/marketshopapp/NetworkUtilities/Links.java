6f58739e74eaf4bf37932d86c3f677830e4413b5
ilias.karditsis <elias.barbarosa@gmail.com>
2014-08-07 18:02:40 +0300

    Commit before leaving from work 07/08/2014

--- app/src/main/java/MarketApp/barbarosa/marketshopapp/MarketApp/barbarosa/marketshopapp/NetworkUtilities/Links.java
+++ app/src/main/java/MarketApp/barbarosa/marketshopapp/MarketApp/barbarosa/marketshopapp/NetworkUtilities/Links.java
@@ -4,23 +4,20 @@ package MarketApp.barbarosa.marketshopapp.MarketApp.barbarosa.marketshopapp.Netw
  * Created by ilias.karditsis on 7/8/2014.
  */
 public class Links {
-    private String serverUrl='37.6.110.14';
-    private String loginJSON='login.php';
-
+    private String serverUrl="37.6.110.14";
+    private String loginJSON="login.php";
+    private String databasePath="database_connect";
+    private int port=81;
 
     public String getServerUrl() {
         return serverUrl;
     }
 
-    public void setServerUrl(String serverUrl) {
-        this.serverUrl = serverUrl;
-    }
+
 
     public String getLoginJSON() {
         return loginJSON;
     }
 
-    public void setLoginJSON(String loginJSON) {
-        this.loginJSON = loginJSON;
-    }
+
 }
